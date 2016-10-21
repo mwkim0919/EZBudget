2b2c89c31d7da5a40a053ec714801059378632f2
Minwoo Kim <mwkim0919@gmail.com>
2016-10-20 17:08:39 -0700

    front-end start.

--- public/javascripts/EZBudget.js
+++ public/javascripts/EZBudget.js
@@ -1,12 +1,1 @@
-angular.module('EZBudget')
-  .config(['$locationProvider', '$routeProvider', function config($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-
-    $routeProvider
-      .when('/signin', {
-        template: '<signin></signin>'
-      })
-
-      .otherwise('/');
-    }
-  ]);
+angular.module('EZBudget', []);
--- public/javascripts/components/signin-component.js
+++
@@ -1,6 +0,0 @@
-angular.module('EZBudget').component('signin', {
-	template: 'Hello, {{$ctrl.user}}!',
-	controller: function GreetUserController() {
-		this.user = 'world';
-	}
-});
